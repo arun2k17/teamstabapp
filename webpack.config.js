fcb71959f8850fb0d24cc5184f3afacba4d2754b
Arunkumar Manickam <armanick@armanick-mac.redmond.corp.microsoft.com>
2022-07-08 12:41:18 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
